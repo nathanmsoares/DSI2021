4f45ad5376c830023970555f45d4972953537c39
nathan <natha@class>
2021-10-05 01:15:52 +0000

    done with CRUD API

--- src/main/java/br/univille/nathandsi2021/api/ProdutoControllerAPI.java
+++ src/main/java/br/univille/nathandsi2021/api/ProdutoControllerAPI.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +53,27 @@ public class ProdutoControllerAPI {
 
     @GetMapping("/{idd}")
     public ResponseEntity<Produto> getById(@PathVariable("idd") Produto produto){
-        return new ResponseEntity<>(produto, HttpStatus.OK);
+        try{
+            return new ResponseEntity<>(produto, HttpStatus.OK);
+        }catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+
+    @DeleteMapping("/{idd}")
+    public ResponseEntity<Produto> delete(@PathVariable("idd") Produto produto){
+        service.delete(produto);
+        return new ResponseEntity<Produto>(produto,HttpStatus.OK);
+    }
+    @PutMapping("/{idd}")
+    public ResponseEntity<Produto> update (@RequestBody Produto produto, 
+                                           @PathVariable("idd") Produto produtoAntigo){
+        produtoAntigo.setDimensao(produto.getDimensao());
+        produtoAntigo.setNome(produto.getNome());
+        produtoAntigo.setPeso(produto.getPeso());
+        service.save(produtoAntigo);
+        return new ResponseEntity<Produto>(produtoAntigo,HttpStatus.OK);
     }
 
 
