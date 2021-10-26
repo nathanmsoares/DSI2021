25729646009eebb79015b2ae4ded957679fb1d6f
nathan <natha@class>
2021-10-26 01:19:08 +0000

    JWT almost done

---
+++ src/main/java/br/univille/nathandsi2021/api/AuthenticationControllerAPI.java
@@ -0,0 +1,31 @@
+package br.univille.nathandsi2021.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+import br.univille.nathandsi2021.model.Usuario;
+import br.univille.nathandsi2021.security.JWTUtil;
+import br.univille.nathandsi2021.service.impl.MyUserDetailsServiceimpl;
+ 
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationControllerAPI {
+ 
+    @Autowired
+    private MyUserDetailsServiceimpl serviceMyUserDetail;
+    @Autowired
+    private JWTUtil serviceJWT;
+ 
+    @PostMapping("/signin")
+    public ResponseEntity signin(@RequestBody Usuario usuario){
+        Usuario usuarioValido = serviceMyUserDetail.buscaUsuarioSenha(usuario.getUsuario(), usuario.getSenha());
+        UserDetails userDetails = serviceMyUserDetail.loadUserByUsername(usuarioValido.getUsuario());
+        String token = serviceJWT.generateToken(userDetails);
+        return ResponseEntity.ok(token);
+    }
+}
--- src/main/java/br/univille/nathandsi2021/security/SecurityConfigurer.java
+++ src/main/java/br/univille/nathandsi2021/security/SecurityConfigurer.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import br.univille.nathandsi2021.service.impl.MyUserDetailsServiceimpl;
@@ -18,14 +20,20 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     @Autowired
     private MyUserDetailsServiceimpl myUserDetailService;
 
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
         .authorizeRequests()
-        .antMatchers("/fonte_dados/**").permitAll()
+        .antMatchers("/fonte_dados/**","/api/v1/auth/signin").permitAll()
         .anyRequest().authenticated().and().formLogin();
+        // .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         
+        // httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
-        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**");
+        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**","/api/v1/auth/signin");
         httpSecurity.headers().frameOptions().sameOrigin();
     }
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
